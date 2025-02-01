f4de93343c759f1140250459af3a52e103ffca24
Newsletter25 <jiayue.zhan@hotmail.com>
2025-01-31 21:16:22 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4610,7 +4610,7 @@ function update() {
   console.log(guess);
 
   if (!guessList.includes(guess)) {
-    document.getElementById("answer").innerText = "NOT IN WORD LIST!";
+    document.getElementById("answer").innerText = "PAST DANS LA LISTE DE MOTS!";
     return;
   }
 
